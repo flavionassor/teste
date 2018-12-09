5fa6b748d3163423e15610fb0ed1866a9a770752
flavionassor <40429223+flavionassor@users.noreply.github.com>
2018-12-09 18:40:21 -0200

    www

--- www/php/db.php
+++
@@ -1,4 +0,0 @@
-<?php
- header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("db4free.net:3306","flavionassor","Fla020891vio","trabbccifsul6p") or die ("could not connect database");
-?>
